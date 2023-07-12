02f5cb04bd1c948094710c04951b595ee01b05d3
Ethan Han <ethanhan@google.com>
2023-07-13 00:37:01 +0800

    fix: loop through all message

--- jit/core/jit_cleaner.py
+++ jit/core/jit_cleaner.py
@@ -1,5 +1,8 @@
 import json
 from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+from typing import MutableSequence
 
 import google.auth
 import googleapiclient.discovery
@@ -12,10 +15,41 @@ from jit.utils import constant
 from jit.utils.logger import jit_logger
 
 
+def republish(pubsub_topic_name, message_data, jit_origin):
+    publisher = pubsub_v1.PublisherClient()
+    future = publisher.publish(
+      pubsub_topic_name,
+      message_data,
+      origin=jit_origin
+    )
+    jit_logger.info(
+      "putting back to queue. jit_origin=%s, message_data=%s, msg_id=%s",
+      jit_origin, message_data, future.result()
+    )
+
+
 def modify_policy_remove_member(conf: config.JitConfig, target_project: str,
   role: str, member: str,
   start: datetime, end: datetime):
-    """Removes a  member from a role binding."""
+    """
+    Removes a member from a role binding.
+    If removed return true, if not existing return false
+    :param conf:
+    :param target_project:
+    :param role:
+    :param member:
+    :param start:
+    :param end:
+    :return: is_success, error_message
+
+    :raises:
+        ~google.auth.exceptions.DefaultCredentialsError:
+            If no credentials were found, or if the credentials found were
+            invalid.
+        ~google.auth.exceptions.MutualTLSChannelError:
+            if there are any problems
+            setting up mutual TLS channel.
+    """
     jit_logger.info(
       "modify_policy_remove_member. config=%s, target_project=%s, role=%s, member=%s, start=%s, end=%s",
       conf, target_project, role, member, start, end
@@ -33,11 +67,14 @@ def modify_policy_remove_member(conf: config.JitConfig, target_project: str,
       .execute()
     )
     jit_logger.debug(
-      "retrived policy. policy=%s, project=%s", policy, target_project
+      "retrieved policy. policy=%s, project=%s", policy, target_project
     )
     expression = f"(request.time >= timestamp(\"{start}\") && request.time < timestamp(\"{end}\"))"
     if len(policy.get("bindings", [])) == 0:
-        jit_logger.error("project binding empty. ")
+        jit_logger.warn(
+          "project binding empty. config=%s, target_project=%s, role=%s, member=%s, start=%s, end=%s",
+          conf, target_project, role, member, start, end
+        )
         return
     binding_count = 0
     for b in policy["bindings"]:
@@ -51,7 +88,12 @@ def modify_policy_remove_member(conf: config.JitConfig, target_project: str,
             b["members"].remove(member)
             binding_count += 1
             continue
-
+    if not binding_count:
+        jit_logger.warn(
+          "policy binding not found. config=%s, target_project=%s, role=%s, member=%s, start=%s, end=%s",
+          conf, target_project, role, member, start, end
+        )
+        return
     policy["bindings"][:] = [b for b in policy["bindings"] if b["members"]]
 
     service.projects().setIamPolicy(
@@ -59,9 +101,26 @@ def modify_policy_remove_member(conf: config.JitConfig, target_project: str,
       body={"policy": policy}
     ).execute()
     jit_logger.info("%d binding has been removed.", binding_count)
+    return
 
 
-def process_pubsub_msg(conf, received_message):
+def process_pubsub_msg(conf: config.JitConfig,
+  received_message: MutableSequence["ReceivedMessage"]) -> bool:
+    """
+    if message origin is not binding, we directly ack it, to clean up the pubsub
+
+    if message expired, call modify_policy_remove_member to remove the binding.
+
+    if current datetime > publishTime + 6 days
+    then republish the message, and ack original msg
+
+    if not expire, don't acknowledge,
+    as we dont expect it to appear in the queue before ack timeout.
+    :param conf:
+    :param received_message:
+    :return: is_ack bool
+    """
+
     jit_logger.info(f"Received: {received_message.message.data}.")
     jit_logger.info(f"attributes: {received_message.message.attributes}")
     origin = received_message.message.attributes.get("origin", "")
@@ -71,9 +130,9 @@ def process_pubsub_msg(conf, received_message):
       origin) != constant.MessageOrigin.BINDING:
         jit_logger.error(
           f"origin invalid: origin={origin}, msg data = {received_message.message.data}.")
-        return
+        return True
 
-        # if any of these process invalid, it may raise exception,
+    # if any of these process invalid, it may raise exception,
     # the exception will be captured in run_jit_cleaner level
     pubsub_msg_dict = json.loads(received_message.message.data)
     target_project_id = pubsub_msg_dict["project_id"]
@@ -83,32 +142,30 @@ def process_pubsub_msg(conf, received_message):
     role = pubsub_msg_dict["role"]
 
     start = expression.get("start", "1900-01-01T00:00:00.00000Z")
-    # start_datetime = datetime.fromisoformat(start)
-    start_datetime = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%fZ")
     end = expression.get("end", "1900-01-01T00:00:00.00000Z")
-    # end_datetime = datetime.fromisoformat(end)
-    end_datetime = datetime.strptime(end, "%Y-%m-%dT%H:%M:%S.%fZ")
+    end_datetime = datetime.strptime(end, "%Y-%m-%dT%H:%M:%S.%fZ").replace(
+      tzinfo=timezone.utc)
 
-    # filter by date.
-    # 1,2,3,4
-    #
-    if end_datetime < datetime.utcnow():
+    publish_time = received_message.message.publish_time.astimezone(
+      timezone.utc)
+    time_for_now = datetime.now(timezone.utc)
+    if end_datetime < time_for_now:
         jit_logger.info(f"access expired: {received_message.message}")
         modify_policy_remove_member(conf, target_project_id, role, member,
                                     start, end)
+    elif publish_time + timedelta(days=6) < time_for_now:
+        republish(conf.pubsub_topic_name, received_message.message.data,
+                  constant.MessageOrigin.BINDING.value)
     else:
-        publisher = pubsub_v1.PublisherClient()
-        future = publisher.publish(
-          conf.pubsub_topic_name,
-          received_message.message.data,
-          origin=origin
-        )
-        jit_logger.info("putting back to queue. msg_id=%s", future.result())
+        # rest of the cases are not expired cases, we dont do ack
+        return False
+    return True
 
 
 def run_jit_cleaner(conf: config.JitConfig):
     """
-    jit_cleaner fetches message from pubsub with max=NUM_MESSAGES
+    jit_cleaner fetches message from pubsub with max=NUM_MESSAGES by loop
+    until it consume all the message in pubsub
     only process the origin is jit-approval which is stored in pubsub message attribute
     based on condition start and expire, decide whether need to clean up
     or republish.
@@ -123,37 +180,47 @@ def run_jit_cleaner(conf: config.JitConfig):
 
     # Wrap subscriber in a 'with' block to automatically call close() when done.
     with subscriber:
-        # The subscriber pulls a specific number of messages. The actual
-        # number of messages pulled may be smaller than max_messages.
-        response = subscriber.pull(
-          request={"subscription": conf.pubsub_subscription_path,
-                   "max_messages": conf.num_messages},
-          retry=retry.Retry(deadline=300),
-        )
 
-        if len(response.received_messages) == 0:
-            jit_logger.info(
-              f"Received 0 message."
+        while True:
+            # The subscriber pulls a specific number of messages. The actual
+            # number of messages pulled may be smaller than max_messages.
+            response = subscriber.pull(
+              request={"subscription": conf.pubsub_subscription_path,
+                       "max_messages": conf.num_messages},
+              retry=retry.Retry(deadline=300),
             )
-            return
 
-        ack_ids = []
-        for received_message in response.received_messages:
-            # in the design doc,
-            # filter based on origin.
-            # process based on expire. if false ignore.
-            #
-            try:
-                process_pubsub_msg(conf, received_message)
-            except Exception as e:
-                jit_logger.info("process pubsub exception: ", e)
+            if len(response.received_messages) == 0:
+                jit_logger.info(
+                  f"Received 0 message. PubSub subscription({conf.pubsub_subscription_path}) is empty."
+                )
+                break
+
+            ack_ids = []
+            for received_message in response.received_messages:
+                try:
+                    is_ack = process_pubsub_msg(conf, received_message)
+                    ack_ids += [received_message.ack_id] if is_ack else []
+                except (google.auth.exceptions.MutualTLSChannelError,
+                        google.auth.exceptions.DefaultCredentialsError) as google_ex:
+                    # if google api exception,
+                    # we should not ack it, and make it reappear.
+                    jit_logger.error("process pubsub google api exception: ",
+                                     google_ex)
+                    continue
+                except Exception as e:
+                    # if general exception, basically data format issues.
+                    # we directly ignore the message and ack it.
+                    jit_logger.error("process pubsub exception: ", e)
+
+            if not ack_ids:
                 continue
-            ack_ids.append(received_message.ack_id)
-        # Acknowledges the received messages so they will not be sent again.
-        subscriber.acknowledge(
-          request={"subscription": conf.pubsub_subscription_path,
-                   "ack_ids": ack_ids}
-        )
-        jit_logger.info(
-          f"Received and acknowledged {len(response.received_messages)} messages from {conf.pubsub_subscription_path}."
-        )
+
+            # Acknowledges the received messages so they will not be sent again.
+            subscriber.acknowledge(
+              request={"subscription": conf.pubsub_subscription_path,
+                       "ack_ids": ack_ids}
+            )
+            jit_logger.info(
+              f"Received and acknowledged {len(response.received_messages)} messages from {conf.pubsub_subscription_path}."
+            )
