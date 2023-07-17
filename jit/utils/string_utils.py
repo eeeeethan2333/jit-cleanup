8669e86d31c01052da5a4742413c79e41ea5f8dd
Mark Statham <mstatham@google.com>
2023-07-17 08:18:07 +0000

    fix: remove milliseconds to avoid issues with precision

--- jit/utils/string_utils.py
+++ jit/utils/string_utils.py
@@ -6,12 +6,15 @@ def try_parsing_date(s, fmt_list):
     """
     parse string s into datetime format using fmt options in fmt_list
     :param s:
-    :param fmt_list: ["%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%dT%H:%M:%SZ"]
+    :param fmt_list: ["%d/%m/%YT%H:%M:%SZ", "%Y-%m-%dT%H:%M:%SZ"]
     :return:
     """
+    # Get rid of the microsecond as some datetime implementation use 8 digit precision
+    datetime_seconds = s.split(".", 1)
+
     for fmt in fmt_list:
         try:
-            return datetime.strptime(s, fmt).replace(tzinfo=timezone.utc)
+            return datetime.strptime(datetime_seconds[0]+'Z', fmt).replace(tzinfo=timezone.utc)
         except ValueError:
             pass
     raise ValueError('no valid date format found')
